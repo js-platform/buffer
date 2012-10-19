651df9603fa7814b6c280b44665ed9dd38f0e831
Alan Kligman <alan.kligman@gmail.com>
2012-10-19 11:17:05 -0400

    Unit tests for copy().

--- tests/specs/copy.spec.js
+++ tests/specs/copy.spec.js
@@ -25,7 +25,35 @@ describe("copy", function() {
       this.buffer.copy(this.target, 1);
     });
     it("should copy entire original buffer to target offset", function() {
+      expect(this.target.__bytes__[0]).toEqual(0);
       expect(this.target.__bytes__.slice(1)).toBeLike(this.buffer.__bytes__);
+    });
+    it("should not have the same array buffer", function() {
+      expect(this.target.__bytes__.buffer).not.toBe(this.buffer.__bytes__.buffer);
+    });
+  });
+  describe("with target buffer, target start offset and source start offset", function() {
+    beforeEach(function() {
+      this.target = new B(5);
+      this.buffer.copy(this.target, 1, 1);
+    });
+    it("should copy the original buffer starting at the offset", function() {
+      expect(this.target.__bytes__[0]).toEqual(0);
+      expect(this.target.__bytes__.slice(1, 4)).toBeLike(this.buffer.__bytes__.slice(1, 4));
+    });
+    it("should not have the same array buffer", function() {
+      expect(this.target.__bytes__.buffer).not.toBe(this.buffer.__bytes__.buffer);
+    });
+  });
+  describe("with target buffer, target start offset, and source start and end offsets", function() {
+    beforeEach(function() {
+      this.target = new B(5);
+      this.buffer.copy(this.target, 1, 1, 3);
+    });
+    it("should copy the original buffer starting at the offset", function() {
+      expect(this.target.__bytes__[0]).toEqual(0);
+      expect(this.target.__bytes__.slice(1, 3)).toBeLike(this.buffer.__bytes__.slice(1, 3));
+      expect(this.target.__bytes__[4]).toEqual(0);
     });
     it("should not have the same array buffer", function() {
       expect(this.target.__bytes__.buffer).not.toBe(this.buffer.__bytes__.buffer);
